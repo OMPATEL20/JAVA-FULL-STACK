5c94ee4d10f352d271bffce4b8070064490486fc
Om Patel <Om.Patel1@einfochips.com>
2023-02-08 16:59:11 +0530

    Day 3 task-1 completed

--- ACTIVITY/DAY2/sumofoddnumbers.java
+++ ACTIVITY/DAY2/sumofoddnumbers.java
@@ -1,26 +1,23 @@
 import java.util.Scanner;
 
 public class sumofoddnumbers {
-public static void main(String[] args)
-{
-    Scanner s=new Scanner(System.in);
-    System.out.println("enter number");
-    int number=s.nextInt();
-if(number < 0){
-    System.out.println("Number is too small");
- }
- else if(number > 32767)
-  {
-     System.out.println("Number is too large");
-    }
-      int sum = 0;
-    while(number%10!=0)
-      {
-        int digit= number%10;
-          if(digit%2!=0)
-         sum+=digit;
-            number=number/10;
-         }
-          System.out.println("The sum of odd digits is " +sum);
-          }}
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter number");
+        int number = s.nextInt();
+        if (number < 0) {
+            System.out.println("Number is too small");
+        } else if (number > 32767) {
+            System.out.println("Number is too large");
 
+        }
+        int sum = 0;
+        while (number % 10 != 0) {
+            int digit = number % 10;
+            if (digit % 2 != 0)
+                sum += digit;
+            number = number / 10;
+        }
+        System.out.println("The sum of odd digits is " + sum);
+    }
+}
