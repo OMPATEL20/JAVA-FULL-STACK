297105eb187c98b8273ade4b3d76502006daab09
Om Patel <Om.Patel1@einfochips.com>
2023-02-13 12:12:30 +0530

    DAY 5 actitivities completed.

---
+++ ACTIVITY/DAY 5/EX 3/CustomException.java
@@ -0,0 +1,13 @@
+
+
+public class CustomException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public CustomException(String msg) {
+		// TODO Auto-generated constructor stub
+		super(msg);
+	}
+}
---
+++ ACTIVITY/DAY 5/EX 4/TeamNameNotFoundException.java
@@ -0,0 +1,12 @@
+
+public class TeamNameNotFoundException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public TeamNameNotFoundException(String msg) {
+		// TODO Auto-generated constructor stub
+		super(msg);
+	}
+}
